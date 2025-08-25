19086fef816b3870bfe91e3c8909766ddaa9acac
lucas <lucas-rsantos@outlook.com>
2025-08-25 19:51:52 -0300

    muitas atividades

--- terceiro semestre/Manipulação de Dados em Arquivos/manipulando arquivos.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-arquivo1 = open('exemplo', 'w', encoding='utf-8')
-arquivo1.write('Olá, mundo!!!!')
-arquivo1.close()
-
-arquivo1 = open('exemplo', 'r', encoding='utf-8')
-print(arquivo1.read())
